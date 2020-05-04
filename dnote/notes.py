f19201f71c23c5881d3528a6b8c2282663f50aeb
M. Yetisir <yetisir@gmail.com>
2020-05-04 12:27:54 -0700

    adds note indexing in dynamodb

--- dnote/notes.py
+++ dnote/notes.py
@@ -1,31 +1,40 @@
 import tempfile
 import subprocess
 import datetime
+import hashlib
 
 import boto3
+import nltk
 
 
 class NoteTable:
-    def __init__(self, endpoint=None, table_name='dnote'):
-        self.db = boto3.resource('dynamodb', endpoint_url=endpoint)
-        self.table = self.db.Table(table_name)
+    def __init__(self, endpoint='http://localhost:8000', table_name='dnote'):
         self.table_name = table_name
+        self.index_name = f'{table_name}_index'
 
-        self.create_table()
+        self.db = boto3.resource('dynamodb', endpoint_url=endpoint)
+
+        self.create_table(self.table_name)
+        self.table = self.db.Table(self.table_name)
+
+        self.create_table(self.index_name)
+        self.index = self.db.Table(self.index_name)
 
     def add_note(self, text):
         if not text:
             text = self.edit_note()
 
         timestamp = datetime.datetime.now().timestamp()
-        id = hash((text, timestamp))
+        id = hashlib.md5(str((text, timestamp)).encode()).hexdigest()
         note = {
-            'id': int(id),
+            'id': id,
             'text': text,
             'timestamp': int(timestamp),
         }
 
         self.table.put_item(Item=note)
+
+        self.index_note(note)
 
     @staticmethod
     def edit_note():
@@ -34,15 +43,15 @@ class NoteTable:
             tf.seek(0)
             return tf.read()
 
-    def create_table(self):
-        if self.table_name in self.db.meta.client.list_tables()['TableNames']:
+    def create_table(self, table_name):
+        if table_name in self.db.meta.client.list_tables()['TableNames']:
             return
         self.db.create_table(
-            TableName=self.table_name,
+            TableName=table_name,
             AttributeDefinitions=[
                 {
                     'AttributeName': 'id',
-                    'AttributeType': 'N',
+                    'AttributeType': 'S',
                 },
             ],
             KeySchema=[
@@ -57,18 +66,44 @@ class NoteTable:
             },
         )
 
+    def index_note(self, note):
+        tokens = self.tokenize(note['text'])
+        for token in sorted(tokens):
+            id = hashlib.md5(token.encode()).hexdigest()
+
+            i = self.index.update_item(
+                Key={
+                    'id': id,
+                },
+                UpdateExpression=(
+                    'SET note_ids = list_append('
+                    '   if_not_exists(note_ids, :empty_list),'
+                    '   :note_id)'
+                ),
+                ExpressionAttributeValues={
+                    ':note_id': [note['id']],
+                    ':empty_list': [],
+                },
+                ReturnValues='UPDATED_NEW', #temp
+            )
+
+    @staticmethod
+    def tokenize(text):
+        nltk.download('punkt')
+        tokens = nltk.word_tokenize(text)
+        stemmer = nltk.PorterStemmer()
+        stemmed_tokens = [stemmer.stem(token) for token in tokens]
+        partial_tokens = []
+        for token in list(set(tokens + stemmed_tokens)):
+            for i in range(1, len(token) + 1):
+                ngrams = nltk.ngrams(token, i)
+                partial_tokens.extend(ngrams)
+
+        return set(''.join(token) for token in partial_tokens)
+
 
     def find_notes(self):
         print(self.table.scan())
-
-
-def main():
-    table = NoteTable(endpoint='http://localhost:8000')
-    table.add_note('this is a new note!')
-    table.add_note('this is a note!')
-    table.add_note('this is another note!')
-
-    table.push_notes()
 
 
 if __name__ == '__main__':
