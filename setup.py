ad098cedead48487bc81d64f3882da5590b17656
M. Yetisir <yetisir@gmail.com>
2020-05-04 16:50:11 -0700

    adds dev option

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dNote',
-    version='0.0.2',
+    version='0.1.0',
     description='Simple CLI utility for managing notes with AWS',
     url='https://github.com/yetisir/dnote',
     author='M. Yetisir',
@@ -11,7 +11,15 @@ setup(
     install_requires=[
         'boto3>=1.13',
         'nltk>=3.5',
+        'configurator[yaml]>=2.0',
     ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-flake8',
+            'sphinx',
+        ]
+    }
     zip_safe=False,
     entry_points={
         'console_scripts': [
