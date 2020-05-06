aed9acdab4ca6fa77d0a1c097f6dd8d9181b8499
M. Yetisir <yetisir@gmail.com>
2020-05-06 11:07:22 -0700

    updates config for ci testing

--- dnote/aws.py
+++ dnote/aws.py
@@ -2,4 +2,15 @@ import boto3
 
 from .config import config
 
-dynamodb = boto3.resource('dynamodb', endpoint_url=config.dynamodb_endpoint)
+
+# TODO: streamline config settings
+try:
+    dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url=config.dynamodb_endpoint)
+except:
+    dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url=config.dynamodb_endpoint,
+        region=config.dynamodb_region)
+
--- dnote/config.py
+++ dnote/config.py
@@ -1,10 +1,12 @@
 import configurator
 
+# TODO: streamline config settings
 
 def get_config(config_dir='~/.dnote.yml'):
     default_config = configurator.Config({
-        # 'dynamodb_endpoint': None,
-        'dynamodb_endpoint': 'http://localhost:8000',
+        'dynamodb_endpoint': None,
+        #'dynamodb_endpoint': 'http://localhost:8000',
+        'dynamodb_region': 'us-west-2',
     })
 
     user_config = configurator.Config.from_path(config_dir, optional=True)
