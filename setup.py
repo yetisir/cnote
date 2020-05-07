a6715fc790f7ee158f821fb8f6051dbf6a75b9b5
M. Yetisir <yetisir@gmail.com>
2020-05-06 17:38:24 -0700

    updating readthedocs config

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dNote',
-    version='0.2.2',
+    version='0.2.3',
     description='Simple CLI utility for managing notes with AWS',
     url='https://github.com/yetisir/dnote',
     author='M. Yetisir',
