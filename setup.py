40a87e9a349e50d3beeead15d0df68b2b7a8c06f
M. Yetisir <yetisir@gmail.com>
2020-08-15 19:17:40 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dNote',
-    version='0.3.6',
+    version='0.3.7',
     description='Simple CLI utility for managing notes with AWS',
     url='https://github.com/yetisir/dnote',
     author='M. Yetisir',
